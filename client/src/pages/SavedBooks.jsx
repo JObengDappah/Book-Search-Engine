1cc82aea9079f98cd5fbef42161a2c288052c0e3
JObengDappah <134343671+JObengDappah@users.noreply.github.com>
2023-11-13 08:29:16 -0500

    vite config script edit

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -65,10 +65,13 @@ const SavedBooks = () => {
               <Col md="4">
                 <Card key={book.bookId} border='dark'>
                   <a href={book.link} target='_blank' rel='noopener noreferrer'>
+
                     {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
                   </a>
                   <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
+
+                    <Card.Title>
+                      {book.title}</Card.Title>
                     <p className='small'>Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
                     <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
